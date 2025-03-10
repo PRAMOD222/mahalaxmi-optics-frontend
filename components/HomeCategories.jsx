73c281c00d1d2a53893abaca1b8e6978b919b1c7
PRAMOD222 <pramodkesarkar2222@gmail.com>
2025-03-10 16:35:29 +0530

    home products

--- components/HomeCategories.jsx
+++ components/HomeCategories.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
@@ -8,205 +8,244 @@ import { cn } from "@/lib/utils";
 
 
 // Dummy product data
-const categories = [
-    {
-        label: "Best Sellers",
-        emoji: "/cool.svg",
-        products: [
-            {
-                id: 1,
-                image: "/glass1.png",
-                name: "Ray Ban RB 90102R",
-                discount: "20% OFF",
-                originalPrice: "₹22,153",
-                price: "₹20,250",
-                colors: 9,
-            },
-            {
-                id: 2,
-                image: "/glass2.png",
-                name: "Oakley OO 9208",
-                discount: "15% OFF",
-                originalPrice: "₹18,000",
-                price: "₹15,300",
-                colors: 6,
-            },
-            {
-                id: 3,
-                image: "/glass3.png",
-                name: "Gucci GG 0741S",
-                discount: "25% OFF",
-                originalPrice: "₹30,000",
-                price: "₹22,500",
-                colors: 5,
-            },
-            {
-                id: 4,
-                image: "/glass4.png",
-                name: "Versace VE 4361",
-                discount: "10% OFF",
-                originalPrice: "₹25,000",
-                price: "₹22,500",
-                colors: 4,
-            },
-            {
-                id: 5,
-                image: "/glass1.png",
-                name: "Ray Ban RB 90102R",
-                discount: "20% OFF",
-                originalPrice: "₹22,153",
-                price: "₹20,250",
-                colors: 9,
-            },
-            {
-                id: 6,
-                image: "/glass2.png",
-                name: "Oakley OO 9208",
-                discount: "15% OFF",
-                originalPrice: "₹18,000",
-                price: "₹15,300",
-                colors: 6,
-            },
-            {
-                id: 7,
-                image: "/glass3.png",
-                name: "Gucci GG 0741S",
-                discount: "25% OFF",
-                originalPrice: "₹30,000",
-                price: "₹22,500",
-                colors: 5,
-            },
-            {
-                id: 8,
-                image: "/glass4.png",
-                name: "Versace VE 4361",
-                discount: "10% OFF",
-                originalPrice: "₹25,000",
-                price: "₹22,500",
-                colors: 4,
-            },
-        ],
-    },
-    {
-        label: "New Arrivals",
-        emoji: "/cool-1.svg",
-        products: [
-            {
-                id: 1,
-                image: "/glass3.png",
-                name: "Gucci GG 0741S",
-                discount: "25% OFF",
-                originalPrice: "₹30,000",
-                price: "₹22,500",
-                colors: 5,
-            },
-            {
-                id: 2,
-                image: "/glass4.png",
-                name: "Versace VE 4361",
-                discount: "10% OFF",
-                originalPrice: "₹25,000",
-                price: "₹22,500",
-                colors: 4,
-            },
-            {
-                id: 3,
-                image: "/glass1.png",
-                name: "Ray Ban RB 90102R",
-                discount: "20% OFF",
-                originalPrice: "₹22,153",
-                price: "₹20,250",
-                colors: 9,
-            },
-            {
-                id: 4,
-                image: "/glass2.png",
-                name: "Oakley OO 9208",
-                discount: "15% OFF",
-                originalPrice: "₹18,000",
-                price: "₹15,300",
-                colors: 6,
-            },
-            {
-                id: 5,
-                image: "/glass3.png",
-                name: "Gucci GG 0741S",
-                discount: "25% OFF",
-                originalPrice: "₹30,000",
-                price: "₹22,500",
-                colors: 5,
-            },
-            {
-                id: 6,
-                image: "/glass4.png",
-                name: "Versace VE 4361",
-                discount: "10% OFF",
-                originalPrice: "₹25,000",
-                price: "₹22,500",
-                colors: 4,
-            },
-            {
-                id: 7,
-                image: "/glass1.png",
-                name: "Ray Ban RB 90102R",
-                discount: "20% OFF",
-                originalPrice: "₹22,153",
-                price: "₹20,250",
-                colors: 9,
-            },
-            {
-                id: 8,
-                image: "/glass2.png",
-                name: "Oakley OO 9208",
-                discount: "15% OFF",
-                originalPrice: "₹18,000",
-                price: "₹15,300",
-                colors: 6,
-            },
-        ],
-    },
-    {
-        label: "Top Brands",
-        emoji: "/cool-2.svg",
-        products: [
-            {
-                id: 5,
-                image: "/glass1.png",
-                name: "Prada PR 17WS",
-                discount: "30% OFF",
-                originalPrice: "₹35,000",
-                price: "₹24,500",
-                colors: 7,
-            },
-            {
-                id: 6,
-                image: "/glass2.png",
-                name: "Dior DIOR0234",
-                discount: "20% OFF",
-                originalPrice: "₹28,500",
-                price: "₹22,800",
-                colors: 8,
-            },
-        ],
-    },
-];
+// const categories = [
+//     {
+//         label: "Best Sellers",
+//         emoji: "/cool.svg",
+//         products: [
+//             {
+//                 id: 1,
+//                 image: "/glass1.png",
+//                 name: "Ray Ban RB 90102R",
+//                 discount: "20% OFF",
+//                 originalPrice: "₹22,153",
+//                 price: "₹20,250",
+//                 colors: 9,
+//             },
+//             {
+//                 id: 2,
+//                 image: "/glass2.png",
+//                 name: "Oakley OO 9208",
+//                 discount: "15% OFF",
+//                 originalPrice: "₹18,000",
+//                 price: "₹15,300",
+//                 colors: 6,
+//             },
+//             {
+//                 id: 3,
+//                 image: "/glass3.png",
+//                 name: "Gucci GG 0741S",
+//                 discount: "25% OFF",
+//                 originalPrice: "₹30,000",
+//                 price: "₹22,500",
+//                 colors: 5,
+//             },
+//             {
+//                 id: 4,
+//                 image: "/glass4.png",
+//                 name: "Versace VE 4361",
+//                 discount: "10% OFF",
+//                 originalPrice: "₹25,000",
+//                 price: "₹22,500",
+//                 colors: 4,
+//             },
+//             {
+//                 id: 5,
+//                 image: "/glass1.png",
+//                 name: "Ray Ban RB 90102R",
+//                 discount: "20% OFF",
+//                 originalPrice: "₹22,153",
+//                 price: "₹20,250",
+//                 colors: 9,
+//             },
+//             {
+//                 id: 6,
+//                 image: "/glass2.png",
+//                 name: "Oakley OO 9208",
+//                 discount: "15% OFF",
+//                 originalPrice: "₹18,000",
+//                 price: "₹15,300",
+//                 colors: 6,
+//             },
+//             {
+//                 id: 7,
+//                 image: "/glass3.png",
+//                 name: "Gucci GG 0741S",
+//                 discount: "25% OFF",
+//                 originalPrice: "₹30,000",
+//                 price: "₹22,500",
+//                 colors: 5,
+//             },
+//             {
+//                 id: 8,
+//                 image: "/glass4.png",
+//                 name: "Versace VE 4361",
+//                 discount: "10% OFF",
+//                 originalPrice: "₹25,000",
+//                 price: "₹22,500",
+//                 colors: 4,
+//             },
+//         ],
+//     },
+//     {
+//         label: "New Arrivals",
+//         emoji: "/cool-1.svg",
+//         products: [
+//             {
+//                 id: 1,
+//                 image: "/glass3.png",
+//                 name: "Gucci GG 0741S",
+//                 discount: "25% OFF",
+//                 originalPrice: "₹30,000",
+//                 price: "₹22,500",
+//                 colors: 5,
+//             },
+//             {
+//                 id: 2,
+//                 image: "/glass4.png",
+//                 name: "Versace VE 4361",
+//                 discount: "10% OFF",
+//                 originalPrice: "₹25,000",
+//                 price: "₹22,500",
+//                 colors: 4,
+//             },
+//             {
+//                 id: 3,
+//                 image: "/glass1.png",
+//                 name: "Ray Ban RB 90102R",
+//                 discount: "20% OFF",
+//                 originalPrice: "₹22,153",
+//                 price: "₹20,250",
+//                 colors: 9,
+//             },
+//             {
+//                 id: 4,
+//                 image: "/glass2.png",
+//                 name: "Oakley OO 9208",
+//                 discount: "15% OFF",
+//                 originalPrice: "₹18,000",
+//                 price: "₹15,300",
+//                 colors: 6,
+//             },
+//             {
+//                 id: 5,
+//                 image: "/glass3.png",
+//                 name: "Gucci GG 0741S",
+//                 discount: "25% OFF",
+//                 originalPrice: "₹30,000",
+//                 price: "₹22,500",
+//                 colors: 5,
+//             },
+//             {
+//                 id: 6,
+//                 image: "/glass4.png",
+//                 name: "Versace VE 4361",
+//                 discount: "10% OFF",
+//                 originalPrice: "₹25,000",
+//                 price: "₹22,500",
+//                 colors: 4,
+//             },
+//             {
+//                 id: 7,
+//                 image: "/glass1.png",
+//                 name: "Ray Ban RB 90102R",
+//                 discount: "20% OFF",
+//                 originalPrice: "₹22,153",
+//                 price: "₹20,250",
+//                 colors: 9,
+//             },
+//             {
+//                 id: 8,
+//                 image: "/glass2.png",
+//                 name: "Oakley OO 9208",
+//                 discount: "15% OFF",
+//                 originalPrice: "₹18,000",
+//                 price: "₹15,300",
+//                 colors: 6,
+//             },
+//         ],
+//     },
+//     {
+//         label: "Top Brands",
+//         emoji: "/cool-2.svg",
+//         products: [
+//             {
+//                 id: 5,
+//                 image: "/glass1.png",
+//                 name: "Prada PR 17WS",
+//                 discount: "30% OFF",
+//                 originalPrice: "₹35,000",
+//                 price: "₹24,500",
+//                 colors: 7,
+//             },
+//             {
+//                 id: 6,
+//                 image: "/glass2.png",
+//                 name: "Dior DIOR0234",
+//                 discount: "20% OFF",
+//                 originalPrice: "₹28,500",
+//                 price: "₹22,800",
+//                 colors: 8,
+//             },
+//         ],
+//     },
+// ];
 
+
+const baseApi = process.env.NEXT_PUBLIC_BASE_API
 
 
 export default function HomeCategories() {
     const [active, setActive] = useState("Best Sellers");
     const [currentPage, setCurrentPage] = useState(1);
+    const [categories, setCategories] = useState([]);
+    const [productsInfo, setProductsInfo] = useState([]);
 
-    const activeCategory = categories.find((cat) => cat.label === active);
 
-    const totalPages = 25
+
+   
+
+
+
+    const fetchCategories = async () => {
+        try {
+            const response = await fetch(`${baseApi}/categories/`); // Wait for response
+            const data = await response.json(); // Wait for JSON parsing
+            setCategories(data);
+            setActive(data[0].name);
+            fetchProducts(data[0].name);
+            console.log("Categories fetched:", data);
+        } catch (error) {
+            console.log("Error fetching categories:", error);
+        }
+    };
+
+    const fetchProducts = async (category) => {
+        try {
+            const response = await fetch(`${baseApi}/products/getProductsByCategory/${category}?page=${currentPage}`); // Wait for response
+            const data = await response.json(); // Wait for JSON parsing
+            setProductsInfo(data);
+            console.log("Products fetched:", data);
+        } catch (error) {
+            console.log("Error fetching products:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchProducts(active);
+    }, [active, currentPage]);
+    
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
 
     const startPage = Math.max(1, currentPage - 1);
-    const endPage = Math.min(totalPages, currentPage + 1);
+    const endPage = Math.min(productsInfo.totalPages, currentPage + 1);
     const pageNumbers = Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
 
     const onPageChange = () => {
-        if (currentPage < totalPages) {
+        if (currentPage < productsInfo.totalPages) {
             setCurrentPage(currentPage + 1);
         }
     }
@@ -222,18 +261,18 @@ export default function HomeCategories() {
 
             {/* Category Tabs */}
             <div className="flex justify-center mt-6 gap-4">
-                {categories.map(({ label, emoji }) => (
+                {categories.map((item) => (
                     <motion.button
-                        key={label}
-                        onClick={() => setActive(label)}
-                        className={`flex items-center gap-2 px-5 py-2 rounded-md border-2 transition-all text-xs md:text-base ${active === label
+                        key={item._id}
+                        onClick={() => setActive(item.name)}
+                        className={`flex items-center gap-2 px-5 py-2 rounded-md border-2 transition-all text-xs md:text-base ${active === item.name
                             ? "bg-[#763f98] text-white border-[#763f98]"
                             : "bg-gray-100 text-gray-700 border-gray-300"
                             }`}
                         whileHover={{ scale: 1.05 }}
                     >
-                        <Image width={18} height={18} alt="emoji" src={emoji} className="hidden md:block" />
-                        {label}
+                        <Image width={18} height={18} alt="emoji" src={'/cool.svg'} className="hidden md:block" />
+                        {item.name}
                     </motion.button>
                 ))}
             </div>
@@ -246,15 +285,15 @@ export default function HomeCategories() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3 }}
             >
-                {activeCategory?.products.map((product) => (
+                {productsInfo.products?.map((product) => (
                     <div
-                        key={product.id}
+                        key={product._id}
                         className=" text-center "
                     >
                         <div className="border border-[#763f98] flex flex-col justify-between">
                             <div className="flex-1 w-full  aspect-[5/4] flex items-center justify-center overflow-hidden">
                                 <Image
-                                    src={product.image}
+                                    src={`${baseApi}/${product.image}`}
                                     width={150}
                                     height={150}
                                     alt={product.name}
@@ -267,9 +306,9 @@ export default function HomeCategories() {
                             </button>
                         </div>
                         <p className="text-[#763f98] font-semibold mt-2">{product.name}</p>
-                        <p className="text-gray-500 text-sm line-through">{product.originalPrice}</p>
-                        <p className="text-xl font-bold">{product.price}</p>
-                        <p className="text-gray-600 text-sm">AVAILABLE IN {product.colors} COLORS</p>
+                        <p className="text-gray-500 text-sm line-through">Rs {product.price} /-</p>
+                        <p className="text-xl font-bold">Rs {product.discounted_price} /-</p>
+                        <p className="text-sm text-[#763f98]">AVAILABLE IN <br /> {product.colors.length} COLORS</p>
                         <button className="mt-3 bg-[#763f98] text-white px-4 py-2  text-sm md:text-base xl:text-xl font-semibold">
                             GRAB NOW!
                         </button>
@@ -302,13 +341,13 @@ export default function HomeCategories() {
                     ))}
 
                     {/* Ellipsis if more pages exist */}
-                    {currentPage < totalPages - 1 && <span className="text-gray-400">...</span>}
+                    {currentPage < productsInfo.totalPages - 1 && <span className="text-gray-400">...</span>}
 
                     {/* Next Button */}
                     <Button
                         variant="outline"
                         className="px-4 py-2"
-                        disabled={currentPage === totalPages}
+                        disabled={currentPage === productsInfo.totalPages}
                         onClick={() => setCurrentPage(currentPage + 1)}
                     >
                         Next &gt;
--- components/HomeLogoScroller.jsx
+++ components/HomeLogoScroller.jsx
@@ -1,7 +1,8 @@
 "use client";
-import Image from "next/image";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "@/components/ui/carousel"
 
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const logos = [
     "/logo1.png",
@@ -12,26 +13,46 @@ const logos = [
 ];
 
 const LogosScroller = () => {
-    return (
-        <div>
-            <Carousel>
-                <CarouselContent>
-                    {logos.map((logo, index) => (
-                        <CarouselItem key={index} className="basis-1/4">
-                            <Image
-                                width={1000}
-                                height={1000}
-                                src={logo}
-                                alt="Slide 1"
-                                className="w-full h-full object-cover"
-                            />
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
+    const [items, setItems] = useState([...logos, ...logos]); // Duplicate logos initially
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            moveLogos(); // Shift the first logo to the end
+        }, 2000); // Adjust speed as needed
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const moveLogos = () => {
+        setItems((prevItems) => {
+            const firstLogo = prevItems[0]; // Get first logo
+            return [...prevItems.slice(1), firstLogo]; // Move first to last
+        });
+    };
+
+    return (
+        <div className="overflow-hidden w-full bg-white py-4">
+            <motion.div
+                className="flex w-max gap-8"
+                animate={{ x: ["0%", "-100%"] }}
+                transition={{
+                    repeat: Infinity,
+                    duration: 10,
+                    ease: "linear",
+                }}
+            >
+                {items.map((logo, index) => (
+                    <div key={index} className="flex-shrink-0 w-1/5">
+                        <Image
+                            src={logo}
+                            alt={`Logo ${index + 1}`}
+                            width={100}
+                            height={50}
+                            className="object-contain w-full h-auto"
+                        />
+                    </div>
+                ))}
+            </motion.div>
         </div>
     );
 };
