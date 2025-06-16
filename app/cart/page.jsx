cba08fa5b82f39f3712081304f7e6db02e7fbeae
PRAMOD222 <pramodkesarkar2222@gmail.com>
2025-06-16 12:30:57 +0530

    missing imports in cart and checkout page

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/navigation";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import Footer from "@/components/Footer";
+import TopBar from "@/components/TopBar";
 
 const baseApi = process.env.NEXT_PUBLIC_BASE_API;
 
